e3df644ab5891f9f5b5d4374164e5ee66766b116
cyber <cyber9298@gmail.com>
2020-01-11 16:59:04 +0900

    [modify] log change.

--- Batch/AppBuild.sh
+++ Batch/AppBuild.sh
@@ -1,13 +1,24 @@
-#!/bin/sh
+#!/bin/zsh
+
 VERSION=2019.3.0f3
 UNITY=/Applications/Unity/Hub/Editor/$VERSION/Unity.app/Contents/MacOS/Unity
 
-LOG="Logs/logFile.log"
+PLATFORM=Android
 $UNITY \
 	-quit \
 	-batchmode \
 	-projectPath $PWD \
-	-buildTarget android \
+	-buildTarget `echo $PLATFORM | tr '[A-Z]' '[a-z]'` \
 	-executeMethod Example.DoIt \
-	-logFile $LOG
+	-logFile "Logs/$PLATFORM/logFile.log"
+echo "exitcode:"$?
+
+PLATFORM="iOS"
+$UNITY \
+	-quit \
+	-batchmode \
+	-projectPath $PWD \
+	-buildTarget `echo $PLATFORM | tr '[A-Z]' '[a-z]'` \
+	-executeMethod Example.DoIt \
+	-logFile "Logs/$PLATFORM/logFile.log"
 echo "exitcode:"$?
