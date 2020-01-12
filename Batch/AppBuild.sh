aab62bd4521ad6375492f29902059ad5d77e2e36
cyber <cyber9298@gmail.com>
2020-01-12 22:03:50 +0900

    [modify] AppBuild.sh change format. add usage. add argument saction.

--- Batch/AppBuild.sh
+++ Batch/AppBuild.sh
@@ -1,9 +1,54 @@
 #!/bin/zsh
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-PLATFORM=$1
-CONFIG=$2
-SCHEME=$3
-TMP_LOG="Logs/$PLATFORM/logFile.log"
+
+function usage {
+    cat <<EOF
+$(basename ${0}) is a tool for ...
+
+Usage:
+    $(basename ${0}) [<options>]
+
+Options:
+    --platform  -p  Platform [Android | iOS]
+    --config    -c  Configuration Identifier
+    --scheme    -s  Scheme Identifier
+    --entry     -e  Entry Method Name
+    --help      -h  print this
+EOF
+}
+while [ $# -gt 0 ];
+do
+    case ${1} in
+        --platform|-p)
+            PLATFORM=${2}
+            shift
+        ;;
+        --config|-c)
+            CONFIG=${2}
+            shift
+        ;;
+        --scheme|-s)
+            SCHEME=${2}
+            shift
+        ;;
+        --entry|-e)
+            ENTRY_METHOD=${2}
+            shift
+        ;;
+        --help|-h)
+            usage
+            exit 0
+        ;;
+        *)
+			echo "[ERROR] Invalid option '${1}'"
+            usage
+            exit 1
+        ;;
+    esac
+    shift
+done
+
+TMP_LOG="Logs/${PLATFORM}/${CONFIG}_${SCHEME}.log"
 
 . $SCRIPT_DIR/SetUp.sh
 
@@ -15,12 +60,12 @@ fi
 "${UNITY_PATH}" \
 	-quit \
 	-batchmode \
-	-projectPath $PWD \
-	-buildTarget `echo $PLATFORM | tr '[A-Z]' '[a-z]'` \
-	-executeMethod Example.DoIt \
-	-config $CONFIG \
-	-scheme $SCHEME \
-	-logFile $TMP_LOG
+	-projectPath "${PWD}" \
+	-buildTarget `echo "${PLATFORM}" | tr '[A-Z]' '[a-z]'` \
+	-executeMethod "${ENTRY_METHOD}" \
+	-config "${CONFIG}" \
+	-scheme "${SCHEME}" \
+	-logFile "${TMP_LOG}"
 EXIT_CODE=$?
 
 echo "$(<$TMP_LOG)"
