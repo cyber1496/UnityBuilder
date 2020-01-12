6cce207bc1c3d509629102264bb2102e183622a6
cyber <cyber9298@gmail.com>
2020-01-12 19:03:52 +0900

    [modify] WindowsでのAndroidGradleビルド対応

--- Assets/UnityBuilder/Plugins/ExternalToolsKit/AndroidGradleProcessor.cs
+++ Assets/UnityBuilder/Plugins/ExternalToolsKit/AndroidGradleProcessor.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Text;
 using System.IO;
 using System.Diagnostics;
 using UnityEditor;
@@ -14,18 +16,42 @@ namespace UnityBuilder.ExternalToolKit {
             if (!helper.BuildOptions.HasFlag(BuildOptions.AcceptExternalModificationsToPlayer)) {
                 return;
             }
-            string scriptPath = Path.Combine(helper.RootPath, "Assets/UnityBuilder/Plugins/ExternalToolsKit/AndroidGradleProcessor/gradle-build.sh");
-            int result = CallScript(scriptPath, out string message, Path.GetFullPath(helper.OutputPath), EditorUserBuildSettings.development ? "debug/launcher-debug.apk" : "release/launcher-release.apk", helper.OutputExt);
+#if UNITY_EDITOR_WIN
+            string scriptName = "gradle-build.bat";
+#else
+            string scriptName = "gradle-build.sh";
+#endif
+            string scriptPath = ConverPath(Path.Combine(helper.RootPath, $"Assets/UnityBuilder/Plugins/ExternalToolsKit/AndroidGradleProcessor/{scriptName}"));
+            string outputPath = ConverPath(Path.GetFullPath(helper.OutputPath));
+            int result = CallScript(scriptPath, out string message, outputPath, EditorUserBuildSettings.development ? "debug/launcher-debug.apk" : "release/launcher-release.apk", helper.OutputExt);
+            //message = Encoding.UTF8.GetString(Encoding.Default.GetBytes(message));
             if (result != 0) {
                 throw new System.Exception(message);
             }
+            else {
+                Debug.Log(message);
+            }
+        }
+        string ConverPath(string path) {
+#if UNITY_EDITOR_WIN
+            path = path.Replace("/","\\");
+#endif
+            return path;
         }
         public int CallScript(string script, out string message, params string[] args) {
             var p = new Process();
+            string argstr = $"\"{script}\" \"{string.Join("\" \"", args)}\"";
+#if UNITY_EDITOR_WIN
+            p.StartInfo.FileName = Environment.GetEnvironmentVariable("ComSpec");
+            p.StartInfo.Arguments = $"/c \"{argstr}\"";
+#else
             p.StartInfo.FileName = "/bin/bash";
-            p.StartInfo.Arguments = $"-c \"sh \"{script}\" \"{string.Join("\" \"", args)}\"\"";
+            p.StartInfo.Arguments = $"-c \"sh {argstr}\"";
+#endif
+            p.StartInfo.CreateNoWindow = true;
             p.StartInfo.UseShellExecute = false;
             p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.StandardOutputEncoding = Encoding.UTF8;
             p.Start();
             message = p.StandardOutput.ReadToEnd();
             p.WaitForExit();
---
+++ Assets/UnityBuilder/Plugins/ExternalToolsKit/AndroidGradleProcessor/gradle-build.bat
@@ -0,0 +1,14 @@
+chcp 65001
+@echo off
+set SCRIPT_DIR=%~dp0
+cd %~1
+xcopy /Q /Y /I %SCRIPT_DIR%\gradle %~1\gradle > nul
+copy /Y %SCRIPT_DIR%\gradle.properties %~1\gradle.properties > nul
+copy /Y %SCRIPT_DIR%\gradlew %~1\gradlew > nul
+copy /Y %SCRIPT_DIR%\gradlew.bat %~1\gradlew.bat > nul
+start /wait /b cmd /c gradlew assemble
+if %ERRORLEVEL% == 0 (
+	copy /Y %~1\launcher\build\outputs\apk\%~2 %~1%~3 > nul
+) else (
+	exit /B 1
+)
