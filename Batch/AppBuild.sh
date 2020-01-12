650be0a46b8d48cbd92b6e0bdb1160b49d1a0c52
cyber <cyber9298@gmail.com>
2020-01-12 16:30:26 +0900

    [fix] mac sed.

--- Batch/AppBuild.sh
+++ Batch/AppBuild.sh
@@ -6,17 +6,19 @@ if [ "$(uname)" == 'Darwin' ]; then
 fi
 
 # ProjectSettings/ProjectVersion.txt からバージョンを抽出
-UNITY_VERSION=`grep -E 'm_EditorVersion:' ProjectSettings/ProjectVersion.txt | sed -r 's/m_EditorVersion: (.*)$/\1/'`
-
 # OS毎に異なるUnity実行ファイルのパスを取得 todo:インストール先の取得 UnityHubの設定を参照すれば取れそう
 if [ $OS == 'Mac' ]; then
+	# need $brew install gnu-sed
+	UNITY_VERSION=`grep -E 'm_EditorVersion:' ProjectSettings/ProjectVersion.txt | sed -E 's/m_EditorVersion: (.*)$/\1/'`
 	UNITY_PATH="/Applications/Unity/Hub/Editor/$UNITY_VERSION/Unity.app/Contents/MacOS/Unity"
 elif [ $OS == 'Windows_NT' ]; then
+	UNITY_VERSION=`grep -E 'm_EditorVersion:' ProjectSettings/ProjectVersion.txt | sed -r 's/m_EditorVersion: (.*)$/\1/'`
 	UNITY_PATH="/f/Program Files/Unity/$UNITY_VERSION/Editor/Unity.exe"
 else
 	echo "Not supported OS."
 	exit 1
 fi
+echo $UNITY_PATH
 
 PLATFORM=Android
 "${UNITY_PATH}" \
