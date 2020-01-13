3b829f18280e05747074ff90687ebd28c07783d5
cyber <cyber9298@gmail.com>
2020-01-14 00:18:40 +0900

    [add] MenuItems

---
+++ Assets/UnityBuilder/Plugins/StandardKit/MenuItems.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using System.IO;
+
+namespace UnityBuilder.StandardKit {
+    internal static class MenuItems {
+        static string PackageFile { get; } = "Packages/UnityBuilder.unitypackage";
+        static string AssetsRoot { get; } = "Assets/UnityBuilder";
+
+#pragma warning disable IDE0051 // MenuItemはどこからも参照されない
+        [MenuItem("Tools/UnityBuilder/Export Package")]
+        static void ExportPackage() {
+            AssetDatabase.ExportPackage(AssetsRoot, PackageFile,
+                ExportPackageOptions.Interactive |
+                ExportPackageOptions.Recurse |
+                ExportPackageOptions.Default);
+        }
+        [MenuItem("Tools/UnityBuilder/Import Package")]
+        static void ImportPackage() {
+            if (File.Exists(PackageFile)) {
+                AssetDatabase.ImportPackage(PackageFile, true);
+            }
+        }
+#pragma warning restore IDE0051
+    }
+}
